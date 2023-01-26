0eed8690feffe4e835f92154a4b6b45b0ebf81f2
JatinVashisht1 <jatin8085vashisht@gmail.com>
2023-01-26 18:55:54 +0530

    Initial commit. Server running on port 5000

---
+++ server.js
@@ -0,0 +1,11 @@
+import "dotenv/config";
+import express from "express";
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+
+app.listen(PORT, ()=>{
+    console.log(`server up on http://localhost:${PORT}/`);
+})
